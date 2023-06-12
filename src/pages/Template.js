830fe03c8e6557be91b717b0184d760cf62b0e3c
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-06-12 18:44:35 -0400

    Added more styling

--- src/pages/Template.js
+++ src/pages/Template.js
@@ -35,6 +35,13 @@ const ProfileHeader = styled.div`
     display: inline-block;
     z-index: -1;
 `
+
+const FavoriteBtn = styled.button`
+    background: red;
+    border-radius: 5px;
+    padding: 5px;
+    border: none;
+`
 const Template = () => {
     const [loading, setLoading] = useState(true)
     let { id } = useParams();
@@ -111,9 +118,14 @@ const Template = () => {
                     <aside className="left-section">
                         <section className="manage-games">
                             <small>Manage Game</small>
-                            <button onClick={() => setopenModal(true)}>Log {`${info[0].name}`}</button> 
+                            <button onClick={() => setopenModal(true)} className="log-btn">Log {`${info[0].name}`}</button> 
                     {/* Render on click, not on page refresh */}
-                            <button onClick={favoriteGame}>{favorited === true ? 'Favorited' : 'Not Favorited'}</button>
+                            <FavoriteBtn onClick={favoriteGame}><svg xmlns="http://www.w3.org/2000/svg" width="24" 
+                            height="20" viewBox="0 0 24 24">
+                                <path fill="white" 
+                                d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z"/>
+                                </svg>
+                                </FavoriteBtn>
                     {/* <button> + Add to List</button> Implementing 'Lists' feature later */}
                         </section>
                 
