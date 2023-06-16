48f77e0ae961389eec50e0e7178788bffeb400ce
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-06-16 13:04:48 -0400

    Styling the Profile page and added global mixin file

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import { UserAuth } from '../firebase/user_auth'; //Get current user from here
 import '../styles/Profile.scss'
 import styled from 'styled-components'
 import cover from '../img/cover_test.jpg'
-import { doc, setDoc, getDoc, getDocs, updateDoc, collection, query, where, collectionGroup, getCountFromServer } from "firebase/firestore"; 
+import { getDocs, collection, query, where, getCountFromServer } from "firebase/firestore"; 
 import {auth, db} from '../firebase/firebase.js'
 import Picture from '../components/Picture'
 
@@ -29,7 +29,7 @@ const Header = () =>  {
     return(
         <ProfileHeader>
             <div className='user-asset'>
-               <img className='profile-picture' src={user?.photoURL}/>   
+               <img className='profile-picture' src={user?.photoURL} alt="user-profile"/>   
                 <h1 className='user-name'>{user?.displayName}</h1> 
             </div>
         </ProfileHeader>
@@ -86,7 +86,7 @@ const Overview = ({click, favoriteGame}) => {
     
     useEffect(() => {
         fetchCount()
-    }, [])
+    }, [fetchCount])
 
     if(click === 1) {
       return (
@@ -150,13 +150,14 @@ const Game = ({click}) => {
         <section className='game-sect'>
             <aside className='filter-list'>
                 <section>
-                    <h2>Search Bar</h2>
-                    <input/>
+                    <h2 className='filter-title'>Search Bar</h2>
+                    <input className='search-bar' placeholder='Search...'/>
                 </section>
                 
                 <section>
-                    <h2>Status</h2>
+                    <h2 className='filter-title'>Status</h2>
                     <ul>
+                        <li>All</li>
                         <li>In Progress</li>
                         <li>On Hold</li>
                         <li>Completed</li>
@@ -179,11 +180,11 @@ const Game = ({click}) => {
                         {games.map((game) => {
                             return(
                                 <tr key={game.id} className='indv-game-row'>
-                                    <td className='img-col'><img src={game.url.replace('t_thumb', 't_micro')}/></td>
+                                    <td className='img-col'><img src={game.url.replace('t_thumb', 't_micro')} alt="game-logo"/></td>
                                     <td className='name-col'>{game.name}</td>
                                     <td className='status-col'>{game.status}</td>
-                                    <td className='start-col'>{game.start_date == 'undefined' ? '-': game.start_date}</td>
-                                    <td className='end-col'>{game.finish_date == 'undefined' ? '-' : game.finish_date}</td>
+                                    <td className='start-col'>{game.start_date === 'undefined' ? '-': game.start_date}</td>
+                                    <td className='end-col'>{game.finish_date === 'undefined' ? '-' : game.finish_date}</td>
                                     <td className='action-col'><button><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
                                         <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
                                         <circle cx="7" cy="7" r="6.5"/><circle cx="7" cy="7" r=".5"/><circle cx="4" cy="7" r=".5"/>
