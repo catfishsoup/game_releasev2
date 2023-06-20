421a0d0727d4e5ca10f4d36724c44c9fa87f139b
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-06-20 18:55:24 -0400

    Add more fields per game page

--- src/pages/Template.js
+++ src/pages/Template.js
@@ -128,9 +128,17 @@ const Template = () => {
                                 d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z"/>
                                 </svg>
                                 </FavoriteBtn>
-                    {/* <button> + Add to List</button> Implementing 'Lists' feature later */}
+                            <button> + Add to List</button>
                         </section>
-                
+                        <section className="release-dates">
+                            <h2>Release Date</h2>
+                            {/* <ul>
+                                {info[0].involved_companies.map((curr_company) => {
+                                return(<List data={curr_company.company}/>)
+                                })}
+                            </ul> */}
+                        </section>
+
                         <section className="platforms">
                             <h2>Platforms</h2>
                             <ul>
@@ -148,6 +156,16 @@ const Template = () => {
                                 })}
                             </ul>
                         </section>
+
+                        <section className="developers">
+                            <h2>External Links</h2>
+                            <ul>
+                                {info[0].involved_companies.map((curr_company) => {
+                                return(<List data={curr_company.company}/>)
+                                })}
+                            </ul>
+                        </section>
+                        
                     </aside>
                     {/*********/}
                     <section className="main-section">
@@ -164,6 +182,16 @@ const Template = () => {
                         
                         <section className="screenshots"> 
                         <h2>Screenshots</h2>
+                        <div className="modal-cont">
+                           {info[0].screenshots.map((picture) => {
+                            return(<Screenshot data={picture}/>)
+                            })}  
+                        </div>
+                              
+                        </section>
+
+                        <section className="screenshots"> 
+                        <h2>Videos</h2>
                         <div className="modal-cont">
                            {info[0].screenshots.map((picture) => {
                             return(<Screenshot data={picture}/>)
