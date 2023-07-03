768615dc37024eb0e2385e90412f6aec8080f4a8
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-07-03 16:39:10 -0400

    User can filter and search game list

--- src/pages/profile/ProfileGames.js
+++ src/pages/profile/ProfileGames.js
@@ -4,13 +4,25 @@ import { UserAuth } from "../../firebase/user_auth"
 import { db } from '../../firebase/firebase.js'
 
 const ProfileGames = () => {
+    //Keep the original data here 
     const [games, setGames] = useState([])
+
+    //Modify the data here 
+    const [gamesCopy, setgamesCopy] = useState([])
+    const [preSearch, setpreSearch] = useState([])
+    const [filter, setFilter] = useState(false)
     const {user} = UserAuth()
+    const statuses = [ 
+        {id: 1, text: 'All'}, {id: 2, text: 'Interested'}, 
+        {id: 3,text: 'On Hold'}, {id: 4, text: 'Dropped'}, 
+        {id: 5, text: 'In Progress'}, {id: 6, text: 'Completed'}, 
+    ]
     useEffect(() => {
         const fetchGame = () => {
             getDocs(collection(db, 'users', `${user.uid}`, 'games')).then((docs) => {
                 docs.forEach((data) => {
                     setGames(game => [...game, data.data()])
+                    setgamesCopy(game => [...game, data.data()])
                 })
             }).catch(e => {
                 console.log(e)
@@ -19,6 +31,25 @@ const ProfileGames = () => {
         return fetchGame
     }, [])
 
+    const filterStatus = (genre) => {
+        const filtered = games.filter(game => game.status == genre)
+        setgamesCopy(filtered)
+        setpreSearch(filtered)
+        setFilter(true)
+        if(genre === 'All') {
+            setgamesCopy(games)
+            
+            setFilter(false)
+        }
+    }
+
+    const searchGame = (e) => {
+        const search = gamesCopy.filter(game => game.name.toLowerCase().includes(e.target.value.toLowerCase()))
+        setgamesCopy(search)
+        if(e.target.value === '' && filter) {
+            setgamesCopy(preSearch)
+        }
+    }
     return (
         <>
         <h1 className='sub-page-title'>Game List</h1>
@@ -26,17 +57,15 @@ const ProfileGames = () => {
                 <aside className='filter-list'>
                     <section>
                         <h2 className='filter-title'>Search Bar</h2>
-                        <input className='search-bar' placeholder='Search...'/>
+                        <input className='search-bar' placeholder='Search...' onChange={(e) => searchGame(e)}/>
                     </section>
                             
                     <section>
                         <h2 className='filter-title'>Status</h2>
-                        <ul>
-                            <li>All</li>
-                            <li>In Progress</li>
-                            <li>On Hold</li>
-                            <li>Completed</li>
-                            <li>Dropped</li>
+                        <ul className="status-filter">
+                            {statuses.map((status) => {
+                                return(<li onClick={() => filterStatus(status.text)} key={status.id}>{status.text}</li>)
+                            })}
                         </ul>
                     </section>
                 </aside>
@@ -52,7 +81,7 @@ const ProfileGames = () => {
                                     <th>Action</th>
                                 </tr> 
 
-                                {games.map((game) => {
+                                {gamesCopy.map((game) => {
                                     return(
                                         <tr key={game.id} className='indv-game-row'>
                                             <td className='img-col'><img src={game.url.replace('t_thumb', 't_micro')} alt="game-logo"/></td>
@@ -61,7 +90,7 @@ const ProfileGames = () => {
                                             <td className='start-col'>{game.start_date === 'undefined' ? '-': game.start_date}</td>
                                             <td className='end-col'>{game.finish_date === 'undefined' ? '-' : game.finish_date}</td>
                                             <td className='action-col'><button><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
-                                                <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+                                                <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round">
                                                 <circle cx="7" cy="7" r="6.5"/><circle cx="7" cy="7" r=".5"/><circle cx="4" cy="7" r=".5"/>
                                                 <circle cx="10" cy="7" r=".5"/></g></svg>
                                                 </button>
@@ -73,7 +102,6 @@ const ProfileGames = () => {
                             </table>
                         </section>
                     </section>
-
         </>
         
         
