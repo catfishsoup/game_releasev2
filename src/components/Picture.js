2e140006fc07a50d90c551422b5f9ebdd7616ae6
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-07-10 12:09:11 -0400

    Overflow fix

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Picture = ({data, text}) => {
     return(
-        <Link className='indv-pic-cont' to={`../games/${data.id}`} target="_blank">
+        <Link className='ind-pic-cont' to={`../games/${data.id}`} target="_blank">
             <div id={data.id}>
                <img src={data.cover?.url.replace('t_thumb', text)
                             || data.url.replace('t_thumb', text)} className='img-test' alt="game-cover"/>
