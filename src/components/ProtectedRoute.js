914086da49788a2b6efabc0a3ef01c7766cdb95a
Jen Nguyen <jennifernguyen0000@gmail.com>
2023-07-12 13:42:07 -0400

    Add favicon to webpage

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,11 +1,12 @@
 import { UserAuth } from "../firebase/user_auth"
 import { Outlet, useParams } from "react-router-dom";
 import { Warning } from '../pages/Warning'
+import { auth } from "../firebase/firebase";
 
 const ProtectedRoute = () => {
     const { user } = UserAuth();
     const { user_name } = useParams();
-    if(!user) {
+    if(!user || user_name !== auth.currentUser.displayName) {
         return <Warning/>;
     } else {
         return <Outlet/>
